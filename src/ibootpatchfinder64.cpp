99edbecef488c769c0ea8f5f930d3667450e2b98
MRX <keremcem2907@icloud.com>
2025-11-04 21:48:51 +0100

    Initial project setup and CI integration
    Add core source files, headers, CMake build configuration, and scripts for macOS and Linux CI workflows. Includes .gitignore, build script, and license. Enables automated building and packaging for multiple platforms.

---
+++ src/ibootpatchfinder64.cpp
@@ -0,0 +1,225 @@
+//
+//  ibootpatchfinder64.cpp
+//  liboffsetfinder64
+//
+//  Created by tihmstar on 28.07.20.
+//  Copyright © 2020 tihmstar. All rights reserved.
+//
+
+#include "ibootpatchfinder64.hpp"
+
+#include "ibootpatchfinder64_base.hpp"
+#include "ibootpatchfinder64_iOS14.hpp"
+
+
+using namespace std;
+using namespace tihmstar::offsetfinder64;
+using namespace tihmstar::libinsn;
+
+#define IBOOT_STAGE_STR_OFFSET 0x200
+#define IBOOT_MODE_STR_OFFSET 0x240
+#define IBOOT_VERS_STR_OFFSET 0x280
+#define iBOOT_BASE_OFFSET 0x318
+#define iBOOT_14_BASE_OFFSET 0x300
+#define KERNELCACHE_PREP_STRING "__PAGEZERO"
+#define ENTERING_RECOVERY_CONSOLE "Entering recovery mode, starting command prompt"
+#define DEBUG_ENABLED_DTRE_VAR_STR "debug-enabled"
+#define DEFAULT_BOOTARGS_STR "rd=md0 nand-enable-reformat=1 -progress"
+#define DEFAULT_BOOTARGS_STR_13 "rd=md0 -progress -restore"
+#define CERT_STR "Apple Inc.1"
+
+
+ibootpatchfinder64::ibootpatchfinder64(bool freeBuf)
+: patchfinder64(freeBuf)
+{
+    //
+}
+
+ibootpatchfinder64 *ibootpatchfinder64::make_ibootpatchfinder64(const char * filename){
+    bool didConstructSuccessfully = false;
+    int fd = 0;
+    uint8_t *buf = NULL;
+    cleanup([&]{
+        if (fd>0) close(fd);
+        if (!didConstructSuccessfully) {
+            safeFreeConst(buf);
+        }
+    })
+    struct stat fs = {0};
+    size_t bufSize = 0;
+    
+    assure((fd = open(filename, O_RDONLY)) != -1);
+    assure(!fstat(fd, &fs));
+    assure((buf = (uint8_t*)malloc(bufSize = fs.st_size)));
+    assure(read(fd,(void*)buf,bufSize)== bufSize);
+    
+
+    auto ret = make_ibootpatchfinder64(buf, bufSize, true);
+    didConstructSuccessfully = true;
+    return ret;
+}
+
+ibootpatchfinder64 *ibootpatchfinder64::make_ibootpatchfinder64(const void *buffer, size_t bufSize, bool takeOwnership){
+    uint8_t *buf = NULL;
+    uint32_t vers = 0;
+    uint32_t vers_arr[5];
+
+    buf = (uint8_t*)buffer;
+    assure(bufSize > 0x1000);
+    
+    assure(!strncmp((char*)&buf[IBOOT_VERS_STR_OFFSET], "iBoot", sizeof("iBoot")-1));
+    retassure(*(uint32_t*)&buf[0] == 0x90000000, "invalid magic");
+
+    retassure(vers = atoi((char*)&buf[IBOOT_VERS_STR_OFFSET+6]), "No iBoot version found!\n");
+    std::string vers_str = std::string((char*)&buf[IBOOT_VERS_STR_OFFSET+6]);
+    for(int i = 0; i < 5; i++) {
+        std::size_t pos = vers_str.find('.');
+        if(pos != std::string::npos) {
+            vers_str = vers_str.substr(pos + 1, vers_str.size() - 1);
+            vers_arr[i] = atoi((char*)vers_str.c_str());
+        }
+    }
+    debug("iBoot-%d inputted\n", vers);
+
+    if (vers >= 6671) {
+        if(vers >= 7429 && vers < 8419) {
+            printf("iOS 15 iBoot detected!\n");
+        } else if(vers >= 8419 && vers <= 9000) {
+            printf("iOS 16 iBoot detected!\n");
+        } else if(vers < 7429) {
+            printf("iOS 14 iBoot detected!\n");
+        } else {
+            printf("Unknown iOS versioned iBoot detected!\n");
+        }
+        return new ibootpatchfinder64_iOS14(buf,bufSize,takeOwnership);
+    }
+    
+    return new ibootpatchfinder64_base(buf,bufSize,takeOwnership);
+}
+
+ibootpatchfinder64::~ibootpatchfinder64(){
+    //
+}
+
+bool ibootpatchfinder64::has_kernel_load(){
+    reterror("not implemented by provider");
+}
+
+bool ibootpatchfinder64::has_recovery_console(){
+    reterror("not implemented by provider");
+}
+
+std::vector<patch> ibootpatchfinder64::get_sigcheck_patch(){
+    reterror("not implemented by provider");
+}
+
+std::vector<patch> ibootpatchfinder64::get_demotion_patch(){
+    reterror("not implemented by provider");
+}
+
+std::vector<patch> ibootpatchfinder64::get_boot_arg_patch(const char *bootargs){
+    reterror("not implemented by provider");
+}
+
+std::vector<patch> ibootpatchfinder64::get_debug_enabled_patch(){
+    reterror("not implemented by provider");
+}
+
+std::vector<patch> ibootpatchfinder64::get_cmd_handler_patch(const char *cmd_handler_str, uint64_t ptr){
+    reterror("not implemented by provider");
+}
+
+std::vector<patch> ibootpatchfinder64::replace_bgcolor_with_memcpy(){
+    reterror("not implemented by provider");
+}
+
+std::vector<patch> ibootpatchfinder64::get_ra1nra1n_patch(){
+    reterror("not implemented by provider");
+}
+
+std::vector<patch> ibootpatchfinder64::get_unlock_nvram_patch(){
+    reterror("not implemented by provider");
+}
+
+std::vector<patch> ibootpatchfinder64::get_nvram_nosave_patch(){
+    reterror("not implemented by provider");
+}
+
+std::vector<patch> ibootpatchfinder64::get_nvram_noremove_patch(){
+    reterror("not implemented by provider");
+}
+
+std::vector<patch> ibootpatchfinder64::get_freshnonce_patch(){
+    reterror("not implemented by provider");
+}
+
+std::vector<patch> ibootpatchfinder64::get_change_reboot_to_fsboot_patch(){
+    reterror("not implemented by provider");
+}
+
+std::vector<patch> ibootpatchfinder64::local_boot_patch(){
+    reterror("not implemented by provider");
+}
+
+std::vector<patch> ibootpatchfinder64::rootdev_patch(int rootdev) {
+    std::vector<patch> patches;
+    loc_t rdstr = findstr("<dict><key>IOProviderClass</key><string>IOMedia</string><key>IOPropertyMatch</key><dict><key>Partition ID</key><integer>%u</integer></dict></dict>", true);
+    debug("rdstr=%p",rdstr);
+
+    std::string fmt = "<dict ID=\"0\"><key>IOProviderClass</key><string ID=\"1\">IOService</string><key>BSD Name</key><string ID=\"2\">disk0s1s%u</string></dict>";
+    size_t len = strlen(fmt.c_str()) + strlen("disk0s1s10"); // just in case
+    char *newstr = (char *)malloc(len);
+
+    snprintf(newstr, len, fmt.c_str(), rootdev);
+
+    patches.push_back({rdstr,newstr,len});
+    
+    loc_t rdrefstr = find_literal_ref(rdstr);
+    debug("rdrefstr=%p",rdrefstr);
+    
+    vmem iter(*_vmem, rdrefstr);
+    
+    while (--iter != insn::movz);
+    
+    loc_t mov_point = iter;
+    debug("mov_point=%p",mov_point);
+    
+    insn new_mov = insn::new_immediate_movz(iter, rootdev, iter().rd(), 0);
+    uint32_t mov_opcode = new_mov.opcode();
+    patches.push_back({mov_point,&mov_opcode,4});
+    
+    vmem iter2(*_vmem, rdrefstr);
+    
+    while (--iter2 != insn::cbz);
+    
+    loc_t cbz_point = iter2;
+    debug("cbz_point=%p",cbz_point);
+    
+    insn branch = insn::new_immediate_b(iter2, iter2().imm());
+    uint32_t opcode = branch.opcode();
+    
+    patches.push_back({cbz_point,&opcode,4});
+    patches.push_back({cbz_point-4,"\x1f\x20\x03\xd5" /* nop */,4});
+
+    return patches;
+}
+
+std::vector<patch> ibootpatchfinder64::renamed_snapshot_patch(){
+    reterror("not implemented by provider");
+}
+
+std::vector<patch> ibootpatchfinder64::rename_kcache_to_kcachd_patch(){
+    reterror("not implemented by provider");
+}
+
+loc_t ibootpatchfinder64::find_iBoot_logstr(uint64_t loghex, int skip, uint64_t shortdec){
+    reterror("not implemented by provider");
+}
+
+uint32_t ibootpatchfinder64::get_el1_pagesize(){
+    reterror("not implemented by provider");
+}
+
+std::vector<patch> ibootpatchfinder64::get_rw_and_x_mappings_patch_el1(){
+    reterror("not implemented by provider");
+}
---
+++ src/ibootpatchfinder64_iOS14.cpp
@@ -0,0 +1,449 @@
+//
+//  ibootpatchfinder64_iOS14.cpp
+//  liboffsetfinder64
+//
+//  Created by tihmstar on 28.07.20.
+//  Copyright © 2020 tihmstar. All rights reserved.
+//
+
+#include <libgeneral/macros.h>
+#include <stdio.h>
+#include "ibootpatchfinder64_iOS14.hpp"
+
+using namespace std;
+using namespace tihmstar::offsetfinder64;
+using namespace tihmstar::libinsn;
+
+#define iBOOT_BASE_OFFSET 0x300
+#define PROD "effective-production-status-ap"
+
+ibootpatchfinder64_iOS14::ibootpatchfinder64_iOS14(const char *filename)
+    : ibootpatchfinder64_base(filename)
+{
+    _entrypoint = _base = (loc_t)*(uint64_t*)&_buf[iBOOT_BASE_OFFSET];
+    _vmem = new vmem({{_buf,_bufSize,_base, vsegment::vmprot::kVMPROTREAD | vsegment::vmprot::kVMPROTWRITE | vsegment::vmprot::kVMPROTEXEC}});
+    debug("iBoot base at=0x%016llx\n", _base);
+}
+
+ibootpatchfinder64_iOS14::ibootpatchfinder64_iOS14(const void *buffer, size_t bufSize, bool takeOwnership)
+    : ibootpatchfinder64_base(buffer,bufSize,takeOwnership)
+{
+    _entrypoint = _base = (loc_t)*(uint64_t*)&_buf[iBOOT_BASE_OFFSET];
+    _vmem = new vmem({{_buf,_bufSize,_base, vsegment::vmprot::kVMPROTREAD | vsegment::vmprot::kVMPROTWRITE | vsegment::vmprot::kVMPROTEXEC}});
+    debug("iBoot base at=0x%016llx\n", _base);
+}
+
+std::vector<patch> ibootpatchfinder64_iOS14::get_sigcheck_patch(){
+    std::vector<patch> patches;
+    loc_t img4decodemanifestexists;
+    if(_vers >= 7459 && _vers < 8419) {
+            img4decodemanifestexists = _vmem->memmem("\xE8\x03\x00\xAA\xC0\x00\x80\x52\x28\x01\x00\xB4", 12);
+    } else {
+        img4decodemanifestexists = _vmem->memmem("\xE8\x03\x00\xAA\xC0\x00\x80\x52\xE8\x00\x00\xB4", 12); //0x180032144;
+    }
+    debug("img4decodemanifestexists=%p",img4decodemanifestexists);
+    assure(img4decodemanifestexists);
+
+    loc_t img4decodemanifestexistsref = find_call_ref(img4decodemanifestexists);
+    debug("img4decodemanifestexistsref=%p",img4decodemanifestexistsref);
+    assure(img4decodemanifestexistsref);
+
+    vmem iter(*_vmem,img4decodemanifestexistsref);
+    vmem iter2(*_vmem,img4decodemanifestexistsref);
+
+    while(++iter != insn::adr);
+    if((uint8_t)iter().rd() != 2) {
+        while(++iter2 != insn::adr);
+        assure((uint8_t)iter().rd() == 2);
+    }
+    loc_t img4interposercallbackptr = iter().imm();
+    debug("img4interposercallbackptr=%p",img4interposercallbackptr);
+    assure(img4interposercallbackptr);
+
+    loc_t img4interposercallback = _vmem->deref(img4interposercallbackptr);
+    debug("img4interposercallback=%p",img4interposercallback);
+    assure(img4interposercallback);
+
+    vmem iter3(*_vmem,img4interposercallback);
+    while(++iter3 != insn::ret);
+    loc_t img4interposercallbackret = iter3().pc();
+    assure(img4interposercallbackret);
+    debug("img4interposercallbackret=%p",img4interposercallbackret);
+    if(--iter3 == insn::add) {
+        while(--iter3 == insn::ldp);
+        if(iter3() != insn::mov) {
+            while(--iter3 != insn::nop);
+        }
+        loc_t img4interposercallbackmov = iter3().pc();
+        assure(img4interposercallbackmov);
+        debug("img4interposercallbackmov=%p",img4interposercallbackmov);
+        patches.push_back({img4interposercallbackmov, "\x00\x00\x80\xD2" /*mov x0, 0*/, 4});
+        while (++iter3 != insn::ret);
+        while (++iter3 != insn::ret);
+        loc_t img4interposercallbackret2 = iter3().pc();
+        assure(img4interposercallbackret2);
+        debug("img4interposercallbackret2=%p", img4interposercallbackret2);
+        patches.push_back({img4interposercallbackret2 - 4, "\x00\x00\x80\xD2" /*mov x0, 0*/, 4});
+    } else {
+        patches.push_back({img4interposercallbackret - 4, "\x00\x00\x80\xD2" /*mov x0, 0*/, 4});
+        while(--iter3 != insn::b) continue;
+        if(--iter3 != insn::ldp) {
+            while(--iter3 != insn::ldp) continue;
+            if(--iter3 != insn::ldp) {
+                reterror("img4interposercallback couldn't find branch for ret2!");
+            } else {
+                while(--iter3 != insn::mov) continue;
+                loc_t img4interposercallbackmovx20 = iter3().pc();
+                debug("img4interposercallbackmovx20=%p", img4interposercallbackmovx20);
+                patches.push_back({img4interposercallbackmovx20, "\x00\x00\x80\xD2" /*mov x0, 0*/, 4});
+            }
+        }
+    }
+    return patches;
+}
+
+std::vector<patch> ibootpatchfinder64_iOS14::get_demotion_patch(){
+    std::vector<patch> patches;
+    /* always production patch*/
+    debug("prod search");
+    if(!_vmem->memstr(PROD))
+        return patches;
+    loc_t productionStr = _vmem->memstr(PROD);
+    debug("prod done");
+    debug("productionStr=%p\n",productionStr);
+    assure(productionStr);
+    loc_t productionRef = find_literal_ref(productionStr);
+    debug("productionRef=%p\n",productionRef);
+    assure(productionRef);
+    vmem iter(*_vmem,productionRef);
+    while (++iter != insn::bl);
+    ++iter;
+    while (++iter != insn::bl);
+    iter = iter().imm();
+    while (++iter != insn::bl);
+    loc_t demoteRef = iter().imm();
+    if (demoteRef) {
+        iter = demoteRef;
+        while (++iter != insn::b);
+        iter = iter().imm();
+        assure((uint32_t)iter().imm() == 1 || (uint32_t)iter().imm() == 0x100);
+        demoteRef = iter;
+        debug("demoteRef=%p\n",demoteRef);
+        patches.push_back({demoteRef,"\x20\x00\x80\xD2" /*mov x0, 0*/,4});
+        if(++iter != insn::ret) {
+            demoteRef = iter;
+            debug("demoteRef2=%p\n",demoteRef);
+            patches.push_back({demoteRef,"\xD5\x03\x20\x1F" /*nop*/,4});
+        }
+    }
+    return patches;
+}
+
+std::vector<patch> ibootpatchfinder64_iOS14::get_change_reboot_to_fsboot_patch(){
+    std::vector<patch> patches;
+
+    loc_t rebootstr = findstr("reboot", true);
+    debug("rebootstr=%p",rebootstr);
+
+    loc_t rebootrefstr = _vmem->memmem(&rebootstr,sizeof(loc_t));
+    debug("rebootrefstr=%p",rebootrefstr);
+    
+    loc_t rebootrefptr = rebootrefstr+8;
+    debug("rebootrefptr=%p",rebootrefptr);
+    
+    loc_t fsbootstr = findstr("fsboot", true);
+    debug("fsbootstr=%p",fsbootstr);
+
+    patches.push_back({rebootrefstr,&fsbootstr,sizeof(loc_t)}); //rewrite pointer to point to fsboot
+
+    loc_t fsbootrefstr = _vmem->memmem(&fsbootstr,sizeof(loc_t));
+    debug("fsbootrefstr=%p",fsbootrefstr);
+    
+    loc_t fsbootfunction = _vmem->deref(fsbootrefstr+8);
+    debug("fsbootfunction=%p",fsbootfunction);
+    patches.push_back({rebootrefstr+8,&fsbootfunction,sizeof(loc_t)}); //rewrite pointer to point to fsboot
+
+    return patches;
+}
+
+std::vector<patch> ibootpatchfinder64_iOS14::local_boot_patch(){
+    std::vector<patch> patches;
+
+    loc_t debug_uartsstr = findstr("debug-uarts", true);
+    debug("debug-uartsstr=%p", debug_uartsstr);
+
+    loc_t debug_uartsrefstr = find_literal_ref(debug_uartsstr);
+    debug("debug-uartsrefstr=%p",debug_uartsrefstr);
+    
+    vmem iter(*_vmem, debug_uartsrefstr);
+    
+    while (++iter != insn::bl);
+    ++iter;
+    while (++iter != insn::bl);
+    
+    loc_t third_blptr = iter;
+    debug("third_blptr=%p",third_blptr);
+    
+    loc_t localbootfunction = iter().imm();
+    debug("localbootfunction=%p",localbootfunction);
+    
+    patches.push_back({localbootfunction,"\x00\x00\x80\x52" /*mov w0, #0*/,4});
+    patches.push_back({localbootfunction+4,"\xC0\x03\x5F\xD6" /*ret*/,4});
+    
+    return patches;
+}
+
+std::vector<patch> ibootpatchfinder64_iOS14::renamed_snapshot_patch(){
+    std::vector<patch> patches;
+
+    loc_t os_updatestr = findstr("com.apple.os.update-", true);
+    debug("os-updatestr=%p", os_updatestr);
+
+    
+    
+    patches.push_back({os_updatestr,"palera1n.rfsnapshot-",20});
+    
+    return patches;
+}
+
+std::vector<patch> ibootpatchfinder64_iOS14::rename_kcache_to_kcachd_patch(){
+    std::vector<patch> patches;
+
+    loc_t kcachestr = findstr("/System/Library/Caches/com.apple.kernelcaches/kernelcache", true);
+    debug("kcachestr=%p",kcachestr);
+    
+
+    patches.push_back({kcachestr + 56,"d" /*doubt_work*/,1});
+
+    return patches;
+}
+
+
+loc_t ibootpatchfinder64_iOS14::find_iBoot_logstr(uint64_t loghex, int skip, uint64_t shortdec){
+    vmem iter(*_vmem);
+    uint64_t longval = 0;
+    uint64_t shortval = 0;
+
+    while (true) {
+        while (++iter != insn::movz || iter().rd() != 9);
+        longval = iter().imm();
+
+        {
+            vmem prevIter{iter,iter.pc()-4};
+            if (prevIter() == insn::movz && prevIter().rd() == 8) {
+                shortval = prevIter().imm();
+            }
+        }
+        while (++iter == insn::movk && iter().rd() == 9){
+
+            uint64_t curval =  iter().imm();
+            
+            longval += curval;
+        }
+        if (longval == loghex && (shortdec == shortval || shortdec == 0)){
+            if (skip-- == 0) return iter;
+        }
+    }
+    
+    return 0;
+}
+
+
+uint32_t ibootpatchfinder64_iOS14::get_el1_pagesize(){
+    vmem iter(*_vmem);
+
+    while (++iter != insn::msr || iter().special() != insn::tcr_el1);
+    
+    loc_t write_tcr_el1 = iter;
+    debug("write_tcr_el1=%p",write_tcr_el1);
+
+    loc_t ref_write_tcr_el1 = find_call_ref(write_tcr_el1);
+    debug("ref_write_tcr_el1=%p",ref_write_tcr_el1);
+    
+    iter = ref_write_tcr_el1;
+    --iter;
+    
+    assure(iter() == insn::bl);
+    
+    loc_t get_tcr_el1 = iter().imm();
+    debug("get_tcr_el1=%p",get_tcr_el1);
+    
+    iter = get_tcr_el1;
+    
+    while (++iter != insn::ret);
+    loc_t get_tcr_el1_eof = iter;
+    debug("get_tcr_el1_eof=%p",get_tcr_el1_eof);
+    
+    uint64_t tcr_el1_val = find_register_value(get_tcr_el1_eof,0,get_tcr_el1);
+    debug("tcr_el1_val=%p",tcr_el1_val);
+
+    uint8_t TG0 = (tcr_el1_val >> 14) & 0b11;
+    
+    switch (TG0) {
+        case 0b00:
+            return 0x1000;
+        case 0b01:
+            return 0x10000;
+        case 0b10:
+            return 0x4000;
+        default:
+            reterror("invalid TG0=%d",TG0);
+    }
+}
+
+
+std::vector<patch> ibootpatchfinder64_iOS14::get_rw_and_x_mappings_patch_el1(){
+    std::vector<patch> patches;
+
+    uint32_t pagesize = get_el1_pagesize();
+    
+    vmem iter(*_vmem);
+
+    while (++iter != insn::msr || iter().special() != insn::ttbr0_el1);
+    
+    loc_t write_ttbr0_el1 = iter;
+    debug("write_ttbr0_el1=%p",write_ttbr0_el1);
+    
+    while (++iter != insn::ret);
+    loc_t write_ttbr0_el1_eof = iter;
+    debug("write_ttbr0_el1_eof=%p",write_ttbr0_el1_eof);
+
+    uint32_t pageshift = pageshit_for_pagesize(pagesize);
+
+    /*
+     create iboot_base_block_entry (rw)
+     */
+    uint64_t iboot_base_block_entry = find_base();
+
+    //mask off unneeded bits
+    iboot_base_block_entry = pte_vma_to_index(pagesize,2,iboot_base_block_entry);
+    iboot_base_block_entry = pte_index_to_vma(pagesize, 2, iboot_base_block_entry);
+    
+    //set pte bits
+    iboot_base_block_entry |= 0x445;
+    
+    /*
+     create loadaddr_block_entry (rx)
+     */
+    loc_t loadaddr_str = findstr("loadaddr", true);
+    debug("loadaddr_str=%p",loadaddr_str);
+    loc_t loadaddr = 0;
+    while (!loadaddr) {
+        loc_t loadaddr_ref = find_literal_ref(loadaddr_str);
+        debug("loadaddr_ref=%p",loadaddr_ref);
+        vmem iter(*_vmem,loadaddr_ref);
+
+        while (++iter != insn::bl);
+        loc_t loadaddr_ref_firstbl = iter;
+        debug("loadaddr_ref_firstbl=%p",loadaddr_ref_firstbl);
+
+        loadaddr = find_register_value(loadaddr_ref_firstbl, 1, loadaddr_ref);
+    }
+    
+    debug("loadaddr=%p",loadaddr);
+
+    //mask off unneeded bits
+    uint64_t loadaddr_block_entry = pte_vma_to_index(pagesize,2,loadaddr);
+    loadaddr_block_entry = pte_index_to_vma(pagesize, 2, loadaddr_block_entry);
+
+    //set pte bits
+    loadaddr_block_entry |= 0x4c5;
+    
+    uint32_t orig_write_ttbr0_el1_insn_cnt = (write_ttbr0_el1_eof-write_ttbr0_el1+4)/4;
+    bool needs_alignment = ! (orig_write_ttbr0_el1_insn_cnt&1);
+    
+
+    /*
+     create patch
+              ldr        x1, =0x180000445
+              nop
+              ldr        x2, =0x8000004c5
+              stp        x1, x2, [x0, #0x8]
+              dmb        sy
+        ///orig
+              msr        ttbr0_el1, x0
+              isb
+              ret
+        ///orig end
+              dq         0x0000000180000445
+              dq         0x00000008000004c5
+    */
+    uint8_t cinsn = 0;
+    {
+        //ldr        x1, =0x180000445
+        insn pins = insn::new_literal_ldr(0, (5+orig_write_ttbr0_el1_insn_cnt)*4, 1);
+        uint32_t opcode = pins.opcode();
+        patches.push_back({(loc_t)(cinsn++*4),&opcode, sizeof(opcode)});
+    }
+    {
+        //nop
+        insn pins = insn::new_general_nop(0);
+        uint32_t opcode = pins.opcode();
+        patches.push_back({(loc_t)(cinsn++*4),&opcode, sizeof(opcode)});
+    }
+    {
+        //ldr        x2, =0x8000004c5
+        insn pins = insn::new_literal_ldr(0, (5+orig_write_ttbr0_el1_insn_cnt)*4, 2);
+        uint32_t opcode = pins.opcode();
+        patches.push_back({(loc_t)(cinsn++*4),&opcode, sizeof(opcode)});
+    }
+    {
+        //stp        x1, x2, [x0, #0x8]
+        insn pins = insn::new_general_stp_offset(0, 8, 1, 2, 0);
+        uint32_t opcode = pins.opcode();
+        patches.push_back({(loc_t)(cinsn++*4),&opcode, sizeof(opcode)});
+    }
+    //dmb        sy
+    patches.push_back({(loc_t)(cinsn++*4),"\xBF\x3F\x03\xD5",4});
+    
+    uint32_t *orig_opcodes = (uint32_t*)memoryForLoc(write_ttbr0_el1);
+    patches.push_back({(loc_t)(cinsn*4),orig_opcodes,4*orig_write_ttbr0_el1_insn_cnt});cinsn+=orig_write_ttbr0_el1_insn_cnt;
+    
+    patches.push_back({(loc_t)(cinsn*4),&iboot_base_block_entry,sizeof(iboot_base_block_entry)}); cinsn+=sizeof(iboot_base_block_entry)/4;
+    patches.push_back({(loc_t)(cinsn*4),&loadaddr_block_entry,sizeof(loadaddr_block_entry)}); cinsn+=sizeof(loadaddr_block_entry)/4;
+
+    uint32_t fullpatch_size = 0;
+    for (auto p: patches){
+        fullpatch_size += p._patchSize;
+    }
+    
+    /*
+     now find an empty spot for to place the payload
+     */
+    loc_t nopspace = findnops(fullpatch_size/4 + 1, true); //alloc 1 more nop than we need
+    debug("nopspace=%p",nopspace);
+    
+    /*
+     ldp needs to load from 8 byte aligned address
+     if needs_alignment is false, we start at 8 byte aligned address and the data is placed at 8 byte aligned
+     if needs_alignmend is true, then we need to start at 8byte aligned + 4 for the data to be 8 byte aligned
+     */
+    if (((nopspace & 0x4) && !needs_alignment /* we don't need alignment, but we start at an +4 address*/ )
+        || (((nopspace & 0x4) == 0) && needs_alignment) /* we are at 8 byte aligned, but we need fixup*/) {
+        nopspace+=4;
+    }
+    
+    //now fixup payload addresses
+    for (auto &p: patches){
+        p._location += nopspace;
+    }
+    
+    /*
+     finally rewrite write_ttbr0 to jump to payload
+     */
+    for (loc_t iloc = write_ttbr0_el1; iloc<write_ttbr0_el1_eof; iloc+=4) {
+        insn pins = insn::new_general_nop(iloc);
+        uint32_t opcode = pins.opcode();
+        patches.push_back({pins,&opcode,sizeof(opcode)});
+    }
+    
+    {
+        //make last orig insn of write_ttbr0 jump to payload
+        insn pins = insn::new_immediate_b(write_ttbr0_el1_eof, nopspace);
+        uint32_t opcode = pins.opcode();
+        patches.push_back({pins,&opcode,sizeof(opcode)});
+    }
+    
+    
+    return patches;
+}
